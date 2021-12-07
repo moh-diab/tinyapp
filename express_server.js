371069e1b2c6fe3681284d2f1c9b06a86e233e9a
moh-diab <mohdiab@gmail.com>
2021-12-07 18:55:23 -0500

    Add a POST Route to Receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,10 +17,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,6 +41,18 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");  // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
