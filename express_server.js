45b4ab334fb22ac17929ce4050064d82f2ab9b09
moh-diab <mohdiab@gmail.com>
2021-12-07 20:45:25 -0500

    Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -29,7 +29,6 @@ app.get("/urls/:shortURL", (req, res) => {
 
 
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -43,11 +42,19 @@ app.get("/hello", (req, res) => {
 });
 
 
-
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");  // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL]=longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/:${shortURL}`);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+})
 
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
